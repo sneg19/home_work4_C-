9f904ab8687638dedde95ccedc31140e7fc48e66
sneg <sneg.sbg@gmail.com>
2023-01-15 17:38:45 +0300

    even number range

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Int32.Parse(Console.ReadLine()!);
+int count = 1;
+while(count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
